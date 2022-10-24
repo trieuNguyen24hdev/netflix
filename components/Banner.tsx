2936ee2d16ff896048dd3e02ef12df23aeba29f8
trieunguyen <trieunguyen.24hdev@gmail.com>
2022-10-24 15:27:32 +0700

    commit

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,9 +1,29 @@
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { Movie } from "../typing";
+import { baseUrl } from "../constants/movie";
 
-function Banner() {
+interface Props {
+  netflixOriginals: Movie[];
+}
+
+function Banner({ netflixOriginals }: Props) {
+  const [movie, setMovie] = useState<Movie | null>(null);
+  useEffect(() => {
+    setMovie(
+      netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
+    );
+  }, [netflixOriginals]);
+
   return (
     <div>
-      <div></div>
+      <div className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
+      <Image
+          layout="fill"
+          src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+          objectFit="cover"
+        />
+      </div>
     </div>
   );
 }
