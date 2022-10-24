04e9e1f063dd9a1f7612231b62061b1f6c8f97a9
trieunguyen <trieunguyen.24hdev@gmail.com>
2022-10-24 14:15:34 +0700

    commit

---
+++ components/login.tsx
@@ -0,0 +1,18 @@
+
+function login() {
+    return (
+        <div>
+            <div>
+            <img
+          src="https://rb.gy/ulxxee"
+          width={100}
+          height={100}
+          className="cursor-pointer object-contain"
+        />
+        </div>
+        <div></div>
+        </div>
+    )
+}
+
+export default login
