7b1e3d5e87a80d4b0301febb86f596e2c544022a
trieunguyen <trieunguyen.24hdev@gmail.com>
2022-10-24 16:13:22 +0700

    commit

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,8 +1,6 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { Movie } from "../typing";
-import { modalState, movieState } from "../atoms/modalAtom.";
-import { useRecoilState } from "recoil";
 import { baseUrl } from "../constants/movie";
 import { FaPlay } from "react-icons/fa";
 
