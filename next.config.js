4e8e7555e40e0bda6591c55ac39d01837ab09027
trieunguyen <trieunguyen.24hdev@gmail.com>
2022-10-25 11:25:59 +0700

    commit

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
-// /** @type {import('next').NextConfig} */
-const withTM = require("next-transpile-modules")([
-  "@stripe/firestore-stripe-payments",
-]); // pass the modules you would like to see transpiled
-
-module.exports = withTM({
+/** @type {import('next').NextConfig} */
+module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['rb.gy', 'image.tmdb.org'],
-  },
-});
+    domains:  ['image.tmdb.org']
+  }
+}
